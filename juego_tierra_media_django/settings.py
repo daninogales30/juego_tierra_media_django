5773ca16b43c4957c8bf4bd9473169ed0dc37cad
daninogales30 <dnogmat3011@fp.iesromerovargas.com>
2025-02-04 09:01:26 +0100

    Apps añadidas al proyecto

--- juego_tierra_media_django/settings.py
+++ juego_tierra_media_django/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'battle.apps.BattleConfig',
+    'character.apps.CharacterConfig',
+    'equipment.apps.EquipmentConfig',
 ]
 
 MIDDLEWARE = [
