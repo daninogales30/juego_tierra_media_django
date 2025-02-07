301b052f1b1315378f8824b8c2614ef111e23e2a
daninogales30 <dnogmat3011@fp.iesromerovargas.com>
2025-02-07 13:11:48 +0100

    Borro el contenido de urls de batalla y equipamiento

--- battle/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from equipment import views
-
-app_name = 'battle'
-urlpatterns = [
-    path('', views.SimulateBattle.as_view(), name='SimulateBattle'),
-]
--- equipment/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from equipment import views
-
-app_name = 'equipment'
-urlpatterns = [
-    path('', views.CreateEquipment.as_view(), name='CreateEquipment'),
-]
