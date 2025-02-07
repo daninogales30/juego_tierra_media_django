38d3b5efc0480fd14e29c32310d9f766a4ab909f
daninogales30 <dnogmat3011@fp.iesromerovargas.com>
2025-02-07 13:13:46 +0100

    Setting modificado

--- juego_tierra_media_django/settings.py
+++ juego_tierra_media_django/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'battle.apps.BattleConfig',
+    'character.apps.CharacterConfig',
+    'equipment.apps.EquipmentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'battle.apps.BattleConfig',
-    'character.apps.CharacterConfig',
-    'equipment.apps.EquipmentConfig',
 ]
 
 MIDDLEWARE = [
