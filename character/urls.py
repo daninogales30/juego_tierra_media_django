3cab4f4df8b88c562fd09c97b0f9f75d5c63e464
vandal <jgomgar208@fp.iesromerovargas.com>
2025-02-11 09:17:57 +0100

    url cambiada de detalles

--- character/urls.py
+++ character/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 app_name = 'character'
 urlpatterns = [
     path('', views.StartGameView.as_view(), name='start_game'),
-    path('principal_menu/', views.PrincipalMenuView.as_view(), name='principal_menu'),
+    path('principal_menu/<int:pk>', views.PrincipalMenuView.as_view(), name='principal_menu'),
     path('create_character/', views.CreateCharacterView.as_view(), name='create_character'),
     path('equip_weapon/<int:pk>', views.EquipWeaponView.as_view(),name='equip_weapon'),
     path('change_ubication/<int:pk>', views.ChangeUbicationView.as_view(), name='change_ubication'),
