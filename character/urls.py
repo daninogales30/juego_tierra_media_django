e8daa977cec4369255556fb0a4a766e5ac36f7d2
vandal <jgomgar208@fp.iesromerovargas.com>
2025-02-11 08:28:58 +0100

    url de la app character

--- character/urls.py
+++ character/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 
 app_name = 'character'
 urlpatterns = [
-    path('', views.StartGameView.as_view(), name='StartGame'),
+    path('', views.StartGameView.as_view(), name='start_game'),
+    path('principal_menu/', views.PrincipalMenuView.as_view(), name='principal_menu'),
+    path('create_character/', views.CreateCharacterView.as_view(), name='create_character'),
+    path('equip_weapon/<int:pk>', views.EquipWeaponView,name='equip_weapon'),
+    path('change_ubication/<int:pk>', views.ChangeUbicationView.as_view(), name='change_ubication'),
+    path('character_list', views.CharacterListView.as_view(), name='character_list'),
+    path('relacion_create', views.RelacionCreateView, name="relacion_create"),
 ]
