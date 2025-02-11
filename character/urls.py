ccb818fdd1d29bb165b18efcfbf4b8ee9b009867
vandal <jgomgar208@fp.iesromerovargas.com>
2025-02-11 08:38:48 +0100

    urls de la app character

--- character/urls.py
+++ character/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', views.StartGameView.as_view(), name='start_game'),
     path('principal_menu/', views.PrincipalMenuView.as_view(), name='principal_menu'),
     path('create_character/', views.CreateCharacterView.as_view(), name='create_character'),
-    path('equip_weapon/<int:pk>', views.EquipWeaponView,name='equip_weapon'),
+    path('equip_weapon/<int:pk>', views.EquipWeaponView.as_view(),name='equip_weapon'),
     path('change_ubication/<int:pk>', views.ChangeUbicationView.as_view(), name='change_ubication'),
     path('character_list', views.CharacterListView.as_view(), name='character_list'),
-    path('relacion_create', views.RelacionCreateView, name="relacion_create"),
+    path('relacion_create', views.RelacionCreateView.as_view(), name="relacion_create"),
 ]
