edb27dcc14337e67dd58ba520f37f50520c28f3a
Dario <dromdel1910@fp.iesromerovargas.com>
2025-02-17 09:08:02 +0100

    URL de equipment create wiew

--- equipment/urls.py
+++ equipment/urls.py
@@ -5,6 +5,8 @@ from equipment import views
 app_name = 'equipment'
 urlpatterns = [
     path('list/', views.EquipmentListView.as_view(), name='equipment_list'),
-    # path('add/', equipment_view, name='equipment_add'), FALTA CAMBIARLO PORQUE ES UN METODO
+    path('add/', views.EquipmentCreateView.as_view(), name='equipment_add'),
     path('delete/<int:pk>/', views.DeleteEquipmentView.as_view(), name='equipment_delete'),
+
+
 ]
