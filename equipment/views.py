876f87e79b2e66575a208108d8864d9dd48fb4d1
Dario <dromdel1910@fp.iesromerovargas.com>
2025-02-18 08:55:04 +0100

    Modificacion de vistas

--- equipment/views.py
+++ equipment/views.py
@@ -1,6 +1,7 @@
+from django.views import View
 from django.views.generic import ListView, FormView
 from django.urls import reverse_lazy
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from equipment.forms import AssignEquipmentForm
 from equipment.models import Equipment
 
@@ -9,7 +10,7 @@ from equipment.models import Equipment
 
 class EquipmentListView(ListView, FormView):
     model = Equipment
-    template_name = "templates/equipment_list.html"
+    template_name = "equipment_list.html"
     context_object_name = "equipments"
     form_class = AssignEquipmentForm
     success_url = reverse_lazy("equipment:equipment_list")
@@ -20,3 +21,20 @@ class EquipmentListView(ListView, FormView):
 
         character.equipment.add(equipment)
         return redirect(self.success_url)
+
+class AssignEquipmentView(View):
+    template_name = "assign_equipment.html"
+
+    def get(self, request):
+        form = AssignEquipmentForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = AssignEquipmentForm(request.POST)
+        if form.is_valid():
+            character = form.cleaned_data['character']
+            equipment = form.cleaned_data['equipment']
+            character.equipment.add(equipment)
+            character.save()
+            return redirect('equipment:equipment_list')
+        return render(request, self.template_name, {'form': form})
