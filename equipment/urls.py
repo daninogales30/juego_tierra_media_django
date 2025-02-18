2cd1acb0d2c387d37b587dfaa966b3596e3b2b79
daninogales30 <dnogmat3011@fp.iesromerovargas.com>
2025-02-18 08:24:53 +0100

    Error urls solucionado

--- equipment/urls.py
+++ equipment/urls.py
@@ -5,6 +5,4 @@ from equipment import views
 app_name = 'equipment'
 urlpatterns = [
     path('list/', views.EquipmentListView.as_view(), name='equipment_list'),
-    path('add/', views.EquipmentCreateView.as_view(), name='equipment_add'),
-    path('delete/<int:pk>/', views.DeleteEquipmentView.as_view(), name='equipment_delete'),
 ]
