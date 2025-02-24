87bdf520f684d88446b7d6cc98a1439e71f0794d
daninogales30 <dnogmat3011@fp.iesromerovargas.com>
2025-02-24 11:35:34 +0100

    Imagen quitada

--- equipment/models.py
+++ equipment/models.py
@@ -5,7 +5,6 @@ class Equipment(models.Model):
     name = models.CharField(max_length=100, unique=True)
     tipo = models.CharField(max_length=50, choices=[('weapon', 'Weapon'), ('armor', 'Armor')])
     potencia = models.IntegerField()
-    imagen = models.ImageField(upload_to='equipment/', null=True, blank=True)
 
     def es_arma(self):
         return self.tipo.lower() == 'weapon'
