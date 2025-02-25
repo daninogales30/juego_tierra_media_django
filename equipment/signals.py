559c26bbfe69ab94c1b762313dfe3d159a502763
daninogales30 <dnogmat3011@fp.iesromerovargas.com>
2025-02-25 10:58:24 +0100

    Armas añadidas

--- equipment/signals.py
+++ equipment/signals.py
@@ -7,6 +7,10 @@ from .models import Weapon, Armor
 def load_initial_data(sender, **kwargs):
    # Solo agregamos las armas precargadas si no existen ya
    if not Weapon.objects.exists():
-       Weapon.objects.create(name='Espada de Fuego', tipo='weapon', potencia=10, alcance=5)
-       Weapon.objects.create(name='Arco de las Sombras', tipo='weapon', potencia=7, alcance=10)
-       Weapon.objects.create(name='Daga Venenosa', tipo='weapon', potencia=5, alcance=3)
+       Weapon.objects.create(name='arco', tipo='weapon', potencia=40, alcance=10)
+       Weapon.objects.create(name='ballesta', tipo='weapon', potencia=47, alcance=10)
+       Weapon.objects.create(name='espada', tipo='weapon', potencia=45, alcance=4)
+       Weapon.objects.create(name='hacha', tipo='weapon', potencia=34, alcance=2)
+       Weapon.objects.create(name='lanza', tipo='weapon', potencia=20, alcance=7)
+       Weapon.objects.create(name='mazo', tipo='weapon', potencia=25, alcance=3)
+       Weapon.objects.create(name='palanca', tipo='weapon', potencia=15, alcance=1)
